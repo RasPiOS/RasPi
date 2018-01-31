e1c78bb094235f04d0f7dc044e250df853de1719
Caleb Clark <clclark@up.com>
2018-01-30 19:45:36 -0600

    added stuff

---
+++ core/make.sh
@@ -0,0 +1,5 @@
+arm-none-eabi-gcc -mcpu=cortex-a7 -fpic -ffreestanding -c boot.S -o boot.o
+arm-none-eabi-gcc -mcpu=cortex-a7 -fpic -ffreestanding -std=gnu99 -c kernel.c -o kernel.o -O2 -Wall -Wextra
+arm-none-eabi-gcc -T linker.ld -o myos.elf -ffreestanding -O2 -nostdlib boot.o kernel.o
+qemu-system-arm -m 256 -M raspi2 -serial stdio -kernel myos.elf
+
